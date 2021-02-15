ff1ea09247461b69b3c786cb7b43d09fb286583d
Angela M <78354179+velasss@users.noreply.github.com>
2021-02-15 21:20:23 +0700

    Create grade.js

---
+++ grade.js
@@ -0,0 +1,26 @@
+
+// Menghitung Grade
+// INPUT "number" with any value 0-100
+
+// IF "number" equals > 80
+//     PRINT "Grade A"
+// ELSE "number" equals > or = 73
+//     PRINT "Grade B"
+// ELSE "number" equals > or = 60
+//     PRINT "Grade C"
+// ELSE "number" equals < 60
+//     PRINT "Failed"
+
+
+var num = 85;
+
+if (num >= 80) {
+  console.log ("grade A");
+} else if (num >= 73) {
+  console.log("grade B");
+} else if (num >= 60){
+  console.log ("grade C")
+}
+else {
+  console.log("Fail");
+} 
